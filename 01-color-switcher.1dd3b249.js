81fa794cd35af8d3e52ad40bfd09cb0748ce6b6d
LenaK513 <kryvtsovaolena@gmail.com>
2022-08-31 20:39:33 +0000

    Deploying to gh-pages from @ LenaK513/goit-js-hw-09@71f690579cff725040d8e7898fc11da8cf834193 🚀

--- 01-color-switcher.1dd3b249.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-start]");document.querySelector("[data-stop]");e.addEventListener("click",onClickColorChange),setInterval((function(){}),1e3)}();
-//# sourceMappingURL=01-color-switcher.1dd3b249.js.map
--- 01-color-switcher.a3d7a3e4.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]");document.querySelector("[data-stop]");let t=null;e.addEventListener("click",onClickColorChange),t=setInterval((()=>{}),1e3);
-//# sourceMappingURL=01-color-switcher.a3d7a3e4.js.map
