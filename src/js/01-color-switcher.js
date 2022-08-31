9a734a939851f841246869ad30725d63eb1730ce
Olena <kryvtsovaolena@gmail.com>
2022-08-31 23:08:16 +0300

    file

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,15 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', onClickColorChange);
+
+// btnStop.addEventListener('click', () => {
+//   console.log(9);
+// });
+intervalId = setInterval(() => {}, 1000);
